c30129f874ce50b8e77744da48720791f3e47a19
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-06-21 08:48:44 +0000

    Added content to the home page

--- home.php
+++ home.php
@@ -12,22 +12,29 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="home_page.css">
   <link rel="shortcut icon" href="favicon/favicon.ico" type="image/x-icon">
+  
   <title>Web Talk - Home</title>
 </head>
 <body>
-<header>
-    <h1><span>Web</span>Talk</h1>
-    <nav>
-      <ul>
-        <li><a href="#" class="active">Forum</a></li>
-        <li><a href="new-post.php">New</a></li>
-        <li><a href="">Your Posts</a></li>
-        <li><a href="profile.php">Profile</a></li>
-      </ul>
-    </nav>
-</header>
+      <header>
+          <h1><span>Web</span>Talk</h1>
+          <nav>
+            <ul>
+              <li><a href="#" class="active">Forum</a></li>
+              <li><a href="new-post.php">New</a></li>
+              <li><a href="">Your Posts</a></li>
+              <li><a href="profile.php">Profile</a></li>
+            </ul>
+          </nav>
+      </header>
+
+      <main class= "post">
+          <p>No posts available to be displayed.</p>
+          <a href="new-post.php" id="button">Create Post</a>
+      </main>
 </body>
 </html>
 
